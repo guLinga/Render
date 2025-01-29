6e20b56f5fff30b7f4a06a20a214b01d54e9e2b7
guLinga <2634917964@qq.com>
2025-01-29 13:17:17 +0800

    feature: 首次回复消息

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
-const express = require('express')
-const app = express()
-const PORT = process.env.PORT || 3030
+import { WechatyBuilder } from 'wechaty'
+import qrcode from 'qrcode-terminal'
 
-// 你的代码
-
-app.listen(PORT, () => {
-  console.log(`server started on port ${PORT}`)
-})
+class weChaty {
+  bot = null
+  constructor() {
+    this.bot = WechatyBuilder.build({
+      name: 'wechat-assistant', // generate xxxx.memory-card.json and save login data for the next login
+      puppetOptions: {
+        uos: true,
+      },
+    })
+    this.bot.on('scan', (code) => {
+      qrcode.generate(code, { small: true })
+    })
+    this.bot.on('login', (user) => console.log(`User ${user} logged in`))
+    this.bot.on('message', this.onMessage.bind(this))
+  }
+  onMessage(message) {
+    const room = message.room()
+    if (
+      room &&
+      message.payload.roomId ===
+        '@@3028fee88f1f1dba1507039875d73c7f6448fd3c3759fb27b5d79fc97072af13'
+    ) {
+      room.say('你好')
+    }
+  }
+  run() {
+    this.bot.start()
+  }
+}
+new weChaty().run()
