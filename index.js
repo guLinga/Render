8f5e949dc6f0c3a55c60ecb2847aaea87f19055b
guLinga <2634917964@qq.com>
2025-01-29 14:02:08 +0800

    feature: 增加请求

--- index.js
+++ index.js
@@ -1,33 +1,52 @@
 import { WechatyBuilder } from 'wechaty'
 import qrcode from 'qrcode-terminal'
+import express from 'express'
+import cors from 'cors'
+const app = express()
+app.use(cors())
+app.options('*', cors())
 
-class weChaty {
-  bot = null
-  constructor() {
-    this.bot = WechatyBuilder.build({
-      name: 'wechat-assistant', // generate xxxx.memory-card.json and save login data for the next login
-      puppetOptions: {
-        uos: true,
-      },
-    })
-    this.bot.on('scan', (code) => {
-      qrcode.generate(code, { small: true })
-    })
-    this.bot.on('login', (user) => console.log(`User ${user} logged in`))
-    this.bot.on('message', this.onMessage.bind(this))
-  }
-  onMessage(message) {
-    const room = message.room()
-    if (
-      room &&
-      message.payload.roomId ===
-        '@@3028fee88f1f1dba1507039875d73c7f6448fd3c3759fb27b5d79fc97072af13'
-    ) {
-      room.say('你好')
+const port = 4100
+
+app.get('/test', (req, res) => {
+  class weChaty {
+    bot = null
+    constructor() {
+      this.bot = WechatyBuilder.build({
+        name: 'wechat-assistant', // generate xxxx.memory-card.json and save login data for the next login
+        puppetOptions: {
+          uos: true,
+        },
+      })
+      this.bot.on('scan', (code) => {
+        const qrcodeImageUrl = `https://wechaty.js.org/qrcode/${encodeURIComponent(
+          code
+        )}`
+        qrcode.generate(code, { small: true }, (data) => {
+          console.log(data)
+        })
+        res.send(qrcodeImageUrl)
+      })
+      this.bot.on('login', (user) => console.log(`User ${user} logged in`))
+      this.bot.on('message', this.onMessage.bind(this))
+    }
+    onMessage(message) {
+      const room = message.room()
+      if (
+        room &&
+        message.payload.roomId ===
+          '@@3028fee88f1f1dba1507039875d73c7f6448fd3c3759fb27b5d79fc97072af13'
+      ) {
+        room.say('你好')
+      }
+    }
+    run() {
+      this.bot.start()
     }
   }
-  run() {
-    this.bot.start()
-  }
-}
-new weChaty().run()
+  new weChaty().run()
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
